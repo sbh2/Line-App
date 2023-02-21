024653f2189dffcd3f416b0cc24b90e0a7328fd1
Sarah Alharbi <sbalharbi22@twq.idserve.net>
2023-02-21 12:35:37 +0300

    Initial Commit

---
+++ Line3/Line3App.swift
@@ -0,0 +1,17 @@
+//
+//  Line3App.swift
+//  Line3
+//
+//  Created by Sara bayan alharbi on 01/08/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct Line3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
